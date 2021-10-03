d2fa25c0cfdaedb2e88c0a72cbbd61a94b93ce2e
dejandejic <dejood@hotmail.no>
2021-10-03 15:33:55 +0200

    js, meta, register

--- js/register.js
+++ js/register.js
@@ -1,23 +1,30 @@
-const form = document.querySelector(".form-container");
+const form = document.querySelector(".form-container form");
 const username = document.querySelector("#username");
 const nameError = document.querySelector("#nameError");
 const email = document.querySelector("#email");
 const emailError = document.querySelector("#emailError");
+const password = document.querySelector("#password");
+const passError = document.querySelector("#passError");
 
 function validateForm() {
     event.preventDefault();
-
     if (chechkLength(username.value, 0) === true) {
         nameError.style.display = "none";
     }
     else {
-        nameError.style.display ="none";
+        nameError.style.display ="block";
     }
     if (validateEmail(email.value) === true) {
         emailError.style.display = "none";
     }
     else {
         emailError.style.display = "block";
+    }
+	if (chechkLength(password.value, 0) === true) {
+        passError.style.display = "none";
+    }
+    else {
+        passError.style.display ="block";
     }
 }
 
